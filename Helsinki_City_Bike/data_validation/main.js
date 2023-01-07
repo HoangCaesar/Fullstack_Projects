46c011418716982f7a4ee01f7931b345338498d3
Caesar <vuhoang7398@gmail.com>
2023-01-07 10:54:59 +0200

    Data_validation: update main.js - divide each JSON file into 2 smaller ones

--- Helsinki_City_Bike/data_validation/main.js
+++ Helsinki_City_Bike/data_validation/main.js
@@ -1,7 +1,8 @@
 const csv = require('csv-parser');
 const fs = require('fs');
 
-const results = [];
+const resultsPart1 = [];
+const resultsPart2 = [];
 
 // Validate data
 const validateResults = async (data) => {
@@ -23,21 +24,29 @@ const readCSVFile = (csvFilename, jsonFilename) => {
     })
         .pipe(csv({})
             .on('data', (data) => {
-                results.push(data);
+                // Devide into 2 arrays to minimize JSON files ---> to import to the datase
+                if (resultsPart1.length >= 500000) {
+                    resultsPart2.push(data);
+                } else {
+                    resultsPart1.push(data);
+                }
             })
             .on('end', async () => {
-                const data = await validateResults(results)
+                const dataPart1 = await validateResults(resultsPart1);
+                const dataPart2 = await validateResults(resultsPart2);
                 // Convert and create JSON files
-                var dictstring = JSON.stringify(data);
-                fs.writeFile(jsonFilename, dictstring, { encoding: 'utf8' }, (err) => err && console.error(err));
+                var dictstringPart1 = JSON.stringify(dataPart1);
+                var dictstringPart2 = JSON.stringify(dataPart2);
+                fs.writeFile(`${jsonFilename}-part1.json`, dictstringPart1, { encoding: 'utf8' }, (err) => err && console.error(err));
+                fs.writeFile(`${jsonFilename}-part2.json`, dictstringPart2, { encoding: 'utf8' }, (err) => err && console.error(err));
             })
         );
 }
 
 // Execution
-readCSVFile('./csv_files/2021-05.csv', `./json_files/2021-05.json`)
-readCSVFile('./csv_files/2021-06.csv', `./json_files/2021-06.json`)
-readCSVFile('./csv_files/2021-07.csv', `./json_files/2021-07.json`)
+readCSVFile('./csv_files/2021-05.csv', `./json_files/2021-05`)
+readCSVFile('./csv_files/2021-06.csv', `./json_files/2021-06`)
+readCSVFile('./csv_files/2021-07.csv', `./json_files/2021-07`)
 
 
 
