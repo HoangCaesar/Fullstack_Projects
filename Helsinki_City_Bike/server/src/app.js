ce97dc67dd2509ca7de7fe1bce1f4237b2b34189
Caesar <vuhoang7398@gmail.com>
2023-01-20 11:56:32 +0200

    Server: refactor folder structure

--- Helsinki_City_Bike/server/src/app.js
+++ Helsinki_City_Bike/server/src/app.js
@@ -4,7 +4,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
 
-const appRoute = require('./src/routes');
+const appRoute = require('./api/v1/routes');
 
 const app = express();
 
