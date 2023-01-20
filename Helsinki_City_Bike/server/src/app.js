d140e4e86c718c9c3aa41f74a01ce95ea6732c83
Caesar <vuhoang7398@gmail.com>
2023-01-20 12:04:34 +0200

    Server: update version for routes (v1)

--- Helsinki_City_Bike/server/src/app.js
+++ Helsinki_City_Bike/server/src/app.js
@@ -20,6 +20,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api/', appRoute);
+app.use('/api/v1/', appRoute);
 
 module.exports = app;
