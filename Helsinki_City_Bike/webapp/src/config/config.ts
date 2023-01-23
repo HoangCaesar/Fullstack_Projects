0a71ef580ec6e50dcdf33161999aa4bbf752fa66
Caesar <vuhoang7398@gmail.com>
2023-01-23 14:29:36 +0200

    Webapp: add general config

---
+++ Helsinki_City_Bike/webapp/src/config/config.ts
@@ -0,0 +1,14 @@
+// ==============================|| THEME CONFIG  ||============================== //
+
+const config = {
+    defaultPath: '/dashboard/default',
+    i18n: 'en',
+    miniDrawer: false,
+    container: true,
+    mode: 'light',
+    presetColor: 'default',
+    themeDirection: 'ltr',
+};
+
+export default config;
+export const drawerWidth = 260;
