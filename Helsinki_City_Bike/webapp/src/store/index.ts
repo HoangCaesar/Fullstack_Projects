62b8bbddfaf8a507df769cb580cca50f0632e8ba
Caesar <vuhoang7398@gmail.com>
2023-01-23 13:40:03 +0200

    Webapp: config store, root saga

---
+++ Helsinki_City_Bike/webapp/src/store/index.ts
@@ -0,0 +1,21 @@
+import createSagaMiddleware from '@redux-saga/core';
+import { Action, configureStore, ThunkAction } from '@reduxjs/toolkit';
+import rootSaga from './rootSaga';
+
+const sagaMiddleware = createSagaMiddleware();
+
+export const store = configureStore({
+    reducer: {},
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(sagaMiddleware),
+});
+
+sagaMiddleware.run(rootSaga);
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+    ReturnType,
+    RootState,
+    unknown,
+    Action<string>
+>;
