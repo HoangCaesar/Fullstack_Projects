9fcafdbf130fad7e8cb4f609d285e782d0feab34
Caesar <vuhoang7398@gmail.com>
2023-01-24 14:43:31 +0200

    Server: create services for JourneyList

--- Helsinki_City_Bike/server/src/api/v1/controllers/journeyList.controller.js
+++ Helsinki_City_Bike/server/src/api/v1/controllers/journeyList.controller.js
@@ -1,6 +1,22 @@
+const findJourneyList = require('../services/journey-list/findJourneyList');
+
 const getAll = async (req, res, next) => {
+    const { _month, _page, _limit, _sort, _order } = req.query;
     try {
-        console.log(a)
+        if (_month === '') {
+            findJourneyList(undefined, _page, _limit, _sort, _order);
+        } else if (_page === '') {
+            findJourneyList(_month, undefined, _limit, _sort, _order);
+        } else if (_limit === '') {
+            findJourneyList(_month, _page, undefined, _sort, _order);
+        } else if (_sort === '') {
+            findJourneyList(_month, _page, _limit, undefined, _order);
+        } else if (_order === '') {
+            findJourneyList(_month, _page, _limit, _sort, undefined);
+        }
+
+        const list = findJourneyList(_month, _page, _limit, _sort, _order);
+
         res.json({
             status: 'success',
             elements: [
---
+++ Helsinki_City_Bike/server/src/api/v1/services/journey-list/findJourneyList.js
@@ -0,0 +1,13 @@
+
+
+const findJourneyList = (
+    month = '5',
+    page = '1',
+    limit = '15',
+    sort = undefined,
+    order = undefined
+) => {
+    console.log(month);
+};
+
+module.exports = findJourneyList;
