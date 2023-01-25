4749148949ab821fa9e8f28dc27d74395c6b9e1e
Caesar <vuhoang7398@gmail.com>
2023-01-25 20:51:03 +0200

    Webapp: add common model

---
+++ Helsinki_City_Bike/webapp/src/models/common.ts
@@ -0,0 +1,21 @@
+export interface PaginationParams {
+  _month: number;
+  _limit: number;
+  _page: number;
+  _totalRows: number;
+}
+
+export interface ListResponse<T> {
+  data: T[];
+  pagination: PaginationParams;
+}
+
+export interface ListParams {
+  _month?: number;
+  _limit?: number;
+  _page?: number;
+  _sort?: string;
+  _order?: 'asc' | 'desc';
+
+  [key: string]: any
+}
