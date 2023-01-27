5fc79df31fb6190b17c380125657d1480e4ee280
Caesar <vuhoang7398@gmail.com>
2023-01-27 14:30:23 +0200

    Webapp: test journeyList api

--- Helsinki_City_Bike/webapp/src/pages/journey-list/JourneyList.tsx
+++ Helsinki_City_Bike/webapp/src/pages/journey-list/JourneyList.tsx
@@ -1,11 +1,28 @@
+import { useEffect } from 'react';
 
+// Project import
+import { useAppDispatch, useAppSelector } from '../../store/hooks';
+import {
+    selectJourneyFilter,
+    selectJourneyList,
+    selectJourneyLoading,
+    selectJourneyPagination,
+    journeyActions,
+} from './journeyList.slice';
 
 // ==============================|| JOURNEY LIST  ||============================== //
 
 const JourneyList = () => {
-  return (
-    <div>JourneyList</div>
-  )
-}
+    const dispatch = useAppDispatch();
+    const journeyList = useAppSelector(selectJourneyList);
+    const filter = useAppSelector(selectJourneyFilter);
 
-export default JourneyList
+    console.log(journeyList);
+
+    useEffect(() => {
+        dispatch(journeyActions.fetchJourneyList(filter));
+    }, []);
+    return <div>JourneyList</div>;
+};
+
+export default JourneyList;
