c7e5e55933d5db825a35476e4b3189fd9b2b45a9
Caesar <vuhoang7398@gmail.com>
2023-01-27 15:14:53 +0200

    Webapp: create convertTime utils

---
+++ Helsinki_City_Bike/webapp/src/utils/convertTime.ts
@@ -0,0 +1,9 @@
+const convertTime = (time: string) => {
+    const convertTime = new Date(time).toString().split(' ').splice(0, 5);
+
+    const newTime = `${convertTime[0]} ${convertTime[4]} ${convertTime[2]}/${convertTime[1]}/${convertTime[3]}`;
+
+    return newTime;
+};
+
+export default convertTime;
