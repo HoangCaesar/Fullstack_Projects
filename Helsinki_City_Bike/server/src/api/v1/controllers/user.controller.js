55372f5b835a276ae570e64964aacbfc7ac2a1b2
Caesar <vuhoang7398@gmail.com>
2023-02-01 15:31:02 +0200

    Server: add user controller

---
+++ Helsinki_City_Bike/server/src/api/v1/controllers/user.controller.js
@@ -0,0 +1,17 @@
+// ========================================== USER CONTROLLER ===============================================
+
+const register = async (req, res, next) => {
+    try {
+        res.json({
+            status: 'success',
+            data: 'true'
+        })
+    } catch (err) {
+        next(err)
+    }
+};
+
+module.exports = {
+    register
+}
+
