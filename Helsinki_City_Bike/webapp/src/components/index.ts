888f79502964eef0acbf8109a2e9b87e528c8266
Caesar <vuhoang7398@gmail.com>
2023-02-02 00:45:38 +0200

    Webapp: add AnimateButton

--- Helsinki_City_Bike/webapp/src/components/index.ts
+++ Helsinki_City_Bike/webapp/src/components/index.ts
@@ -1,10 +1,11 @@
 export { default as ScrollTop } from './ScrollTop';
 export { default as Loadable } from './Loadable';
 export { default as MainCard } from './MainCard';
-export { default as Transitions } from './extended/Transitions';
 export { default as LogoSection } from './logo/LogoSection';
 export { default as SimpleBar } from './third-party/SimpleBar';
 export { default as Breadcrumbs } from './extended/Breadcrumbs';
+export { default as Transitions } from './extended/Transitions';
+export { default as AnimateButton } from './extended/AnimateButton';
 export { default as JourneyTable } from './journey-list/JourneyTable';
 export { default as JourneyFilter } from './journey-list/JourneyFilter';
 export { default as AnalyticTotal } from './statistics/AnalyticTotal';
