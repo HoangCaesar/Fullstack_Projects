316719339cb0d8bcb4834ff612caee8909ce1e38
Caesar <vuhoang7398@gmail.com>
2023-02-03 23:11:36 +0200

    Webapp: update header content

--- Helsinki_City_Bike/webapp/src/layout/main-layout/header/header-content/HeaderContent.tsx
+++ Helsinki_City_Bike/webapp/src/layout/main-layout/header/header-content/HeaderContent.tsx
@@ -1,25 +1,41 @@
+import { useEffect, useState } from 'react';
 // material-ui
-import { Box, IconButton, Link, useMediaQuery } from '@mui/material';
+import { Box, useMediaQuery } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
-import { GithubOutlined } from '@ant-design/icons';
 
 // project import
-import Profile from './profile/Profile';
-import Notification from './Notification';
+import authHandler from '../../../../utils/authHandler';
 import MobileSection from './MobileSection';
+import Notification from './Notification';
+import Profile from './profile/Profile';
 
 // ==============================|| HEADER - CONTENT ||============================== //
 
 const HeaderContent = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+
     const theme = useTheme();
     const matchesXs = useMediaQuery(theme.breakpoints.down('md'));
 
-    return (
-        <Box sx={{ display: 'flex', alignItems: 'center'}}>
+    useEffect(() => {
+        (async () => {
+            const isTokenAvailable = await authHandler();
+            if (isTokenAvailable) setIsLoggedIn(true);
+        })();
+    }, []);
+    return isLoggedIn ? (
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
             {matchesXs && <Box sx={{ width: '100%', ml: 1 }} />}
             <Notification />
             {!matchesXs && <Profile />}
             {matchesXs && <MobileSection />}
+        </Box>
+    ) : (
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            {matchesXs && <Box sx={{ width: '100%', ml: 1 }} />}
+            {/* <Notification /> */}
+            {/* {!matchesXs && <Profile />} */}
+            {/* {matchesXs && <MobileSection />} */}
         </Box>
     );
 };
